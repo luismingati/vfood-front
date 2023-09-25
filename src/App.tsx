63ff5ba12f72243b505460281614a224dc9be520
ditthales <tvgf@academy.cin.ufpe.br>
2023-09-25 18:17:23 -0300

    adjusting things

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Router } from "./router";
 export function App() {
   return (
     <>
-      <ColaboratorHeader name={"Alice Martins"} role={"Manager"} stars={4.5}/>
+      <Router/>
     </>
   );
 }
