8f277190b582a9e1a349d24b45d836fb2b609b84
humilimo <lfoc@cin.ufpe.br>
2023-10-04 01:25:50 -0300

    star centralized

--- src/components/NoteCard/NoteCard.tsx
+++ src/components/NoteCard/NoteCard.tsx
@@ -5,7 +5,7 @@ const NoteCard = (props:NoteCardModel) => {
     const viewModel = useNoteCardViewModel(props)
 
     return(
-        <div className={`flex flex-row ${viewModel.card.long ? "w-[105px] justify-center items-center text-[24px]" : "w-[69px] text-[18px]"} text-white ${viewModel.card.note !== undefined && !viewModel.card.long ? "items-start justify-start" : "items-center justify-center"} py-[4px] px-[4px] gap-[4px] rounded-[10px] ${viewModel.card.bg}`}>
+        <div className={`flex flex-row ${viewModel.card.long ? "w-[105px] justify-center items-center text-[24px]" : "w-[69px] text-[18px]"} text-white ${viewModel.card.note != undefined && !viewModel.card.long ? "items-start justify-start" : "items-center justify-center"} py-[4px] px-[4px] gap-[4px] rounded-[10px] ${viewModel.card.bg}`}>
             <p className="material-symbols-outlined">star</p>
             {viewModel.card.note !== undefined && (<p className="font-poppins font-bold">{viewModel.card.note}</p>)}
         </div>
