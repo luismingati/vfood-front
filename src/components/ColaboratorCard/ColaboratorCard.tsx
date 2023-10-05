bcd226fb01bf721d2d9473c7624b74aa4b989370
humilimo <lfoc@cin.ufpe.br>
2023-10-05 15:10:31 -0300

    colaborator card now fixed

--- src/components/ColaboratorCard/ColaboratorCard.tsx
+++ src/components/ColaboratorCard/ColaboratorCard.tsx
@@ -9,13 +9,13 @@ const ColaboratorCard = (props: ColaboratorCardModel) => {
 
   return (
     <Link to={`/colaborators/${viewModel.card.id}`}>
-      <div className="min-w-[162px] h-[182px] flex flex-col text-slate-800 gap-[2px] px-[20px] py-[10px] bg-neutral-50 rounded-[10px] justify-center items-center cursor-pointer">
+      <div className="w-[162px] h-[182px] flex flex-col text-slate-800 gap-[2px] px-[20px] py-[10px] bg-neutral-50 rounded-[10px] justify-center items-center cursor-pointer">
         <img
           src={viewModel.card.avatar ? viewModel.card.avatar : avatarIcon}
           alt="avatar"
           className="w-[48px] h-[48px] rounded-full"
         />
-        <p className="font-poppins text-[18px] font-bold">
+        <p className="font-poppins text-[18px] font-bold w-[162px] text-center">
           {viewModel.card.name}
         </p>
         <p className="font-poppins text-[14px]">{viewModel.card.role}</p>
